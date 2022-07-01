8ec84e477aface4b93a9f25cad900a9f17e7032d
andrii-venher <vengerandr@gmail.com>
2022-07-02 00:37:17 +0200

    Update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,8 +9,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 export default {
   env: process.env.NODE_ENV,
+  bot: {
+    token: process.env.BOT_TOKEN || '',
+  },
   mongo: {
     connectionString: process.env.MONGO_CONNECTION_STRING || '',
     db: process.env.MONGO_DB_NAME || '',
+    collections: {
+      users: 'users',
+      subscriptions: 'subscriptions',
+      paymentMethods: 'paymentMethods',
+      transactions: 'transactions',
+    },
   },
 };
